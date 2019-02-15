94cf5b4ef791f391bb934c0cd6bcec33d7e2eee5
RedMoon32 <rinat.babichev@gmail.com>
2019-02-15 14:06:28 +0300

    student info

--- week4/common.h
+++ week4/common.h
@@ -4,6 +4,11 @@ typedef struct _test_struct{
     unsigned int b;
 } test_struct_t;
 
+typedef struct{
+	char *name;
+	unsigned int age;
+	char *group;
+} student;
 
 typedef struct result_struct_{
 
